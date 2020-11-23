7bcd1deff0df5faeaa15be2e48488474fa5079ef
Kohji Yamamoto <k.yamamoto03@iforcom.jp>
2020-11-23 11:41:17 +0900

    プロジェクト ファイルを追加します。

---
+++ API/Function1.cs
@@ -0,0 +1,52 @@
+using DailyCouter.Shared;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API
+{
+    public static class Function1
+    {
+        [FunctionName("WeatherForecast")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            var randomNumber = new Random();
+            var temp = 0;
+
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = temp = randomNumber.Next(-20, 55),
+                Summary = GetSummary(temp)
+            }).ToArray();
+
+            return new OkObjectResult(result);
+        }
+        private static string GetSummary(int temp)
+        {
+            var summary = "Mild";
+
+            if (temp >= 32)
+            {
+                summary = "Hot";
+            }
+            else if (temp <= 16 && temp > 0)
+            {
+                summary = "Cold";
+            }
+            else if (temp <= 0)
+            {
+                summary = "Freezing";
+            }
+
+            return summary;
+        }
+    }
+}
